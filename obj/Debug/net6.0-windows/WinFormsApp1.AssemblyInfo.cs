4daa24194ab146bbbd4f40ff61db2a1bb0f443de
Phuc <phucthhe172242@fpt.edu.vn>
2024-03-21 11:04:34 +0700

    upload

--- obj/Debug/net6.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8f98730ff21d4e69fd8fb073e98567aba03db9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+275c9c00239097d424d7ef85b9047c8c8fdb1c90")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
